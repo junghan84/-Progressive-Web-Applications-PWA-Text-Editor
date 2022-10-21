4858ddf6c9691a273c79cff6bf234904e3c199fb
JungHan Seo <junghanseo84@gmail.com>
2022-10-21 13:33:38 -0700

    create js Code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+  console.error('putDb not implemented');
+
+  const editorDb = await openDB('jate', 1);
+  const text = editorDb.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const request = store.put({id: 1, value:content});
+  const result =  await request;
+  console.log('Add to Database',result.value);
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+
+  console.error('getDb not implemented');
+  const editorDb = await openDB('jate', 1);
+  const text = editorDb.transaction('jate', 'readonly');
+  const store = text.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Result',result.value)
+
+  return result?.value
+}
 
 initdb();
